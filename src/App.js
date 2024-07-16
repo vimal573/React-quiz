3dc6b1d7c089b2ef4c72d1af596f6854050419f2
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-16 18:16:56 +0530

    Starting a New Quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Questions from './Questions';
 
 const initialState = {
   questions: [],
@@ -16,6 +17,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: 'ready' };
     case 'dataFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
     default:
       throw new Error('Action Unknown');
   }
@@ -48,7 +51,10 @@ function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Questions />}
       </Main>
     </div>
   );
