e37aa24933d98da63a0dc99ad44f2654ea82ffb5
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-16 17:56:25 +0530

    Loading questions from Fake API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action Unknown');
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
+
+  console.log(questions, status);
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataRecieved', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+        console.error(err);
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
-    <div>
-      <h1>HEllo world</h1>
+    <div className="app">
+      <Header />
+
+      <Main></Main>
     </div>
   );
 }
